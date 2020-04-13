0111bea58b81edc1002ad7a33e2d0f136492b1ec
peter-bae <baekok123440@gmail.com>
2020-04-13 14:30:14 +0200

    studentList added

--- Code/app/src/main/java/android/example/dataadapters/MainActivity.java
+++ Code/app/src/main/java/android/example/dataadapters/MainActivity.java
@@ -3,6 +3,8 @@ package android.example.dataadapters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] names = new String[10];
+        names [0] = "Kanya";
+        names [1] = "Peter";
+        names [2] = "Nadine";
+        names [3] = "Paul";
+        names [4] = "Rainer";
+        names [5] = "Felipe";
+        names [6] = "Lourenco";
+        names [7] = "Emiliano";
+        names [8] = "Rodrigo";
+        names [9] = "Danilo";
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, names);
+
+        ListView listView = (ListView) findViewById(R.id.studentList);
+        listView.setAdapter(itemsAdapter);
     }
 }
